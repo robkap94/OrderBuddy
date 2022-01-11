e97247ddbfb9765698e9a6bb4645d85f9f7391bc
Robert Kaptur <rob.kaptur@gmail.com>
2022-01-11 22:27:23 +0100

    Changed name of "main-view"

--- src/main/java/com/robertkaptur/orderbuddy/MainApplication.java
+++ src/main/java/com/robertkaptur/orderbuddy/MainApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("manager-view.fxml"));
         Scene managerView = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Order Buddy - Manager View");
         stage.setScene(managerView);
