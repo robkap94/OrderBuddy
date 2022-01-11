d46feecfeb9617b1eaaf7801bda55c7fa55b9944
Robert Kaptur <rob.kaptur@gmail.com>
2022-01-12 00:11:03 +0100

    Set up an icon for both windows

--- src/main/java/com/robertkaptur/orderbuddy/Window.java
+++ src/main/java/com/robertkaptur/orderbuddy/Window.java
@@ -3,6 +3,7 @@ package com.robertkaptur.orderbuddy;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -13,10 +14,11 @@ public class Window {
     public Window(Stage windowStage, String fxmlLoader, int width, int height, double positionOnScreen, String title) throws IOException {
 
         // Loading of the Manager View's window
-        FXMLLoader windowFxmlLoader = new FXMLLoader(MainApplication.class.getResource(fxmlLoader));
+        FXMLLoader windowFxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/" + fxmlLoader));
         Scene managerView = new Scene(windowFxmlLoader.load(), width, height);
         windowStage.setTitle(title);
         windowStage.setScene(managerView);
+        windowStage.getIcons().add(new Image((String.valueOf(MainApplication.class.getResource("img/OrderBuddy.png")))));
         windowStage.show();
         // Positioning Manager View's window
         Rectangle2D windowViewBounds = Screen.getPrimary().getVisualBounds();
