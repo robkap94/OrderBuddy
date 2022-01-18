a62d1e1250c923ca3ceda6e7b6f94e3e9e3a40f4
Robert Kaptur <rob.kaptur@gmail.com>
2022-01-18 02:35:51 +0100

    Changed OrderData from OrderList + adjusted cntlr

---
+++ src/main/java/com/robertkaptur/orderbuddy/OrderData.java
@@ -0,0 +1,58 @@
+package com.robertkaptur.orderbuddy;
+
+import javafx.collections.ObservableList;
+
+import java.time.format.DateTimeFormatter;
+
+public class OrderData {
+
+    private static OrderData instance = new OrderData();
+    private static String dbFilename = "db/db.txt";
+
+    private DateTimeFormatter formatter;
+    private ObservableList<Order> listOfOrders;
+
+    private OrderData() {
+        formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    }
+
+    public static OrderData getInstance() {
+        return instance;
+    }
+
+    public static String getDbFilename() {
+        return dbFilename;
+    }
+
+    public ObservableList<Order> getListOfOrders() {
+        return listOfOrders;
+    }
+
+    public void addOrder(Order order) {
+        listOfOrders.add(order);
+    }
+
+    public void deleteOrder(Order order) {
+        listOfOrders.remove(order);
+    }
+
+    public void loadDatabase() {
+        /* TODO: Prepare loading sequence:
+        0) Create buffered reader with path
+        1) listOfOrders needs to be initialized as a ObservableArrayList from FXCollections
+        2) Path object which will check (whether exists) and creates when required db file & will mark this path
+        3) Load parts of object into separated vars
+        4) Create object
+        5) Add it into observableArrayList
+        6) Add "finally" to close the bufferedReader
+
+        PLEASE NOTICE TO REBUILD Dates in object creation:
+        a) Date of Creation is CURRENT DATE in ISO_LOCAL_DATE_TIME
+        b) Date of Delivery is NULL*/
+    }
+
+    public void saveDatabase() {
+        /*TODO: Prepare saving sequence:
+        *  DO everything the same as in loadDatabase, but in opposite + utilize BufferedWriter*/
+    }
+}
