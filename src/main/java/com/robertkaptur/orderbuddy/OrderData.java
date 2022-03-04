3afee7b11050a3cbde0ba225ae89e58c33f2e401
Robert Kaptur <rob.kaptur@gmail.com>
2022-03-04 17:43:56 +0100

    Imported SQLite library into project

--- src/main/java/com/robertkaptur/orderbuddy/OrderData.java
+++ src/main/java/com/robertkaptur/orderbuddy/OrderData.java
@@ -21,12 +21,14 @@ public class OrderData {
     private final static OrderData instance = new OrderData(); // Creating one, static, instance of OrderData
     private final static String dbDir = "db";
     private final static String dbFile = "db.txt";
+    private final static String dbName = "database.db";
     private final static String dbFilename = dbDir + "/" + dbFile; // This will go to resources -> db -> db file
     private final static Path path = Paths.get(dbFilename);
     private final DecimalFormat decimalFormatter = new DecimalFormat("#.00", DecimalFormatSymbols.getInstance(Locale.US)); // Utilized during saving db
     private final ObservableList<Order> listOfOrders = FXCollections.observableArrayList(); // Declaring list of orders as JavaFX's observableArrayList
 
     private OrderData() { // Constructor (private) - For Singleton purposes, to ensure that no second OrderData instance will be created
+
         DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME; // TODO: Check how it can be utilized in the future (I don't remember idea behind it)
     }
 
@@ -107,5 +109,11 @@ public class OrderData {
                 bufferedWriter.newLine(); // Ending line, for particular object, and starting line for new object found by iterator in list of orders
             }
         }
+    }
+
+    private void createNewDatabase() {
+        String dbLocation = dbDir + "/" + dbName; // This will go to resources -> db -> db file
+        Path path = Paths.get(dbLocation);
+        String dbUrl = "jdbc:sqlite:" + path.toString();
     }
 }
